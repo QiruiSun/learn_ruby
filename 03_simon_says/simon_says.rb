c6a93049df63df119c42bab91e4ca5dca555b064
Qirui Sun <qiruisun@gmail.com>
2016-01-18 16:37:07 -0500

    create shout repeat titleize first_word start_of_word method, focus on the last method, when the first element(the) of the array was capitalized then fourth (the) is no longer equal to the first element since it became The. in such case, the fourth (the) wont be capitalized

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -5,3 +5,29 @@ end
 def shout(sound)
   "#{sound.to_s.upcase}"
 end
+
+def repeat(sound, n = 2)
+   ("#{sound} " * n).strip
+end
+
+def start_of_word(string, n)
+  string[0..n-1]
+end
+
+def first_word(string)
+  (string.partition(" "))[0]
+end
+
+def titleize(string)
+  string_array = string.split(" ")
+  string_array.each do |x|
+    if x == string_array[0]  # if x == string.split(" ")[0]
+      x.capitalize!
+    elsif x != "the" && x != "over" && x != "on" && x != "and"
+      x.capitalize!
+    else
+      x
+    end
+  end
+ string_array.join(" ")
+end
